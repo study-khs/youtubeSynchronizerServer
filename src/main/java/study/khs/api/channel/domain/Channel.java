a28f24f02279467f31fc34424daf85dbd36fbbd4
scoll <scoll@DESKTOP-AI17LJ7>
2017-02-25 18:12:15 +0900

    entity 추가

--- src/main/java/study/khs/api/channel/domain/Channel.java
+++ src/main/java/study/khs/api/channel/domain/Channel.java
@@ -1,10 +1,12 @@
 package study.khs.api.channel.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import lombok.Data;
 
 @Data
+@Entity
 public class Channel {
 
 	@Id
--- src/main/java/study/khs/api/member/domain/Member.java
+++ src/main/java/study/khs/api/member/domain/Member.java
@@ -1,10 +1,12 @@
 package study.khs.api.member.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import lombok.Data;
 
 @Data
+@Entity
 public class Member {
 
 	@Id
