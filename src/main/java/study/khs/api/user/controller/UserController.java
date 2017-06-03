25233ba5e3a6aba388b0a0c649d872e6675a2052
scollector65 <scollector65@naver.com>
2017-06-03 11:41:12 +0900

    Update UserController.java

--- src/main/java/study/khs/api/user/controller/UserController.java
+++ src/main/java/study/khs/api/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package study.khs.api.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
